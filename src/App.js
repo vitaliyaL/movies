17520ffc483834764f0288a5934f8d3cd647a8de
vitaliyaL <vita0623@mail.ru>
2022-07-18 18:43:59 +0300

    feat: added changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Page from "./page/Page/Page";
 import Info from './page/Info'
+import Favorite from './page/Favorite/Favorite'
 import { Route, Routes } from "react-router-dom";
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
        <Routes>
           <Route path="/" element={<Page />} />
           <Route path="/info/:id" element={<Info />} />
+          <Route path="/favorite" element={<Favorite/>}/>
         </Routes>
     </div>
   );
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,28 +1,20 @@
 import React from "react";
 import "./Card.css";
 import { Link } from "react-router-dom";
-import {addMovies} from '../../redux/favorite/action'
-import { useDispatch ,useSelector} from "react-redux";
 
-function Card({ id, name, img, desc }) {
-  const dispatch= useDispatch()
-  const { movies } = useSelector(
-    (state) => state.movies
-  );
-  const handleFavorite = (e) => {
-    console.log(id);
-    dispatch(addMovies((movies.find(i=>i.id==id))))
-  };
+function Card({ id, name, img, desc, onclick,nameBtn }) {
+  
   return (
     <div className="card" key={id}>
       <Link to={`/info/${id}`}>
         <img src={`https://image.tmdb.org/t/p/w500${img}`} />
       </Link>
-      <button onClick={handleFavorite}>
-        favorite
-      </button>
+      
       <p className="name">{name}</p>
       <p className="desc">{desc}</p>
+      <button onClick={onclick} className="fav" >
+        favorite
+      </button>
     </div>
   );
 }
